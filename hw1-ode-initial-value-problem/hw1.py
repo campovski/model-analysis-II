ec90ef42f70e1ebcf49739b0d12050949927fcc5
campovski <campovski@gmail.com>
2019-03-01 17:34:07 +0100

    HW1 - Add task2 and some additional calculations for task1

--- hw1-ode-initial-value-problem/hw1.py
+++ hw1-ode-initial-value-problem/hw1.py
@@ -7,14 +7,14 @@ import scipy.constants
 
 
 def planetary_motion(y0, t0, t1, dt, G=1, M=1, a=1, omega=1):
-    def sytem(t, ys):
+    def system(t, ys):
         x = ys[0]
         y = ys[1]
         u = ys[2]
         v = ys[3]
         return numpy.vstack((u, v, -G * M * x / (x*x + y*y)**1.5, -G * M * y / (x*x + y*y)**1.5))
 
-    solver = scipy.integrate.ode(sytem).set_integrator("dopri5")
+    solver = scipy.integrate.ode(system).set_integrator("dopri5")
     solver.set_initial_value(y0, t0)
 
     iter_outs = []
@@ -145,8 +145,8 @@ def task1_err_ellipses():
 
 
 def task1_err_to_dt():
-    t0, t1 = 0, 20
-    dt1 = 20
+    t0, t1 = 0, 100
+    dt1 = 7
     N = 10000 * dt1
     dts = numpy.flip(numpy.linspace(0.0001, dt1, N))
     v0 = 1
@@ -200,10 +200,166 @@ def task1_err_to_dt():
     plt.show()
 
 
+def task1_circular_dt(dt):
+    t0, t1 = 0, 1000
+    v0 = 1
+    y0 = [1, 0, 0, v0]
+    solved = planetary_motion(y0, t0, t1, dt)
+
+    plot_trajectory(solved, v0, "trajectory_dt={}".format(dt))
+
+
+def task1_stability():
+    t0, t1 = 0, 1000
+    v0 = 1
+    y0 = [1, 0, 0, v0]
+    dts = [1, 0.1, 0.01, 0.001, 0.0001]
+
+    errors_radius = {}
+    errors_energy = {}
+    errors_angmoment = {}
+
+    E0 = -0.5
+    L0 = y0[0]*y0[3] - y0[1]*y0[2]
+
+    for dt in dts:
+        print(dt)
+
+        sol = planetary_motion(y0, t0, t1*dt, dt)
+
+        x = numpy.array([s[0] for s in sol[:,1]])
+        y = numpy.array([s[1] for s in sol[:,1]])
+        u = numpy.array([s[2] for s in sol[:,1]])
+        v = numpy.array([s[3] for s in sol[:,1]])
+        steps = len(x)
+        errors_radius[dt] = numpy.abs(numpy.ones(steps) - numpy.sqrt(x*x + y*y))
+        errors_energy[dt] = numpy.abs((x*x + y*y) / 2 - 1 - E0*numpy.ones(steps))
+        errors_angmoment[dt] = numpy.abs(x*v - y*u - L0)
+
+    # stability of distance
+    fig = plt.figure()
+    plt.title("Radius error")
+    plots = []
+    for dt in dts:
+        plots += plt.semilogy(range(t1), errors_radius[dt][:t1], linewidth=0.5)
+    plt.legend(plots, dts, loc="upper right")
+    plt.xlabel("iter")
+    plt.ylabel("$r_{\\mathrm{err}} = \\left|\\,1 - \\sqrt{x^2 + y^2}\\,\\right|$")
+    fig.savefig("images/task2_stability_r.pdf", bbox_inches="tight")
+    plt.show()
+
+    # stability of energy
+    fig = plt.figure()
+    plt.title("Energy error")
+    plots = []
+    for dt in dts:
+        plots += plt.semilogy(range(t1), errors_energy[dt][:t1], linewidth=0.5)
+    plt.legend(plots, dts, loc="upper right")
+    plt.xlabel("iter")
+    plt.ylabel("$E_{\\mathrm{err}} = \\left|\\,\\left(\\frac{1}{2}(x^2 + y^2) - 1\\right) - \\frac{1}{2} \\,\\right|$")
+    fig.savefig("images/task2_stability_E.pdf", bbox_inches="tight")
+    plt.show()
+
+    # stability of angular momentum
+    fig = plt.figure()
+    plt.title("Angular momentum error")
+    plots = []
+    for dt in dts:
+        plots += plt.semilogy(range(t1), errors_angmoment[dt][:t1], linewidth=0.5)
+    plt.legend(plots, dts, loc="upper right")
+    plt.xlabel("iter")
+    plt.ylabel("$L_{\\mathrm{err}} = \\left|\\, (xv - yu) - 1 \\,\\right|$")
+    fig.savefig("images/task2_stability_L.pdf", bbox_inches="tight")
+    plt.show()
+
+
+def task1_small_ellipse():
+    t0, t1 = 0, 100
+    v0 = 0.1
+    y0 = [1, 0, 0, v0]
+    dt = 0.1
+    solved = planetary_motion(y0, t0, t1, dt)
+
+    plot_trajectory(solved, v0, "trajectory_v0={0}_dt={1}".format(v0, dt))
+
+################################################################################
+#
+#       TASK 2
+#
+################################################################################
+
+def planetary_motion2(y0, t0, t1, dt):
+    def bypassing_position(t):
+        return -10 + 2*t, 1.5
+
+    def system(t, ys):
+        x = ys[0]
+        y = ys[1]
+        u = ys[2]
+        v = ys[3]
+        xz, yz = bypassing_position(t)
+        return numpy.vstack((u, v, -x / (x*x + y*y)**1.5 - (x - xz) / ( (x - xz)**2 + (y - yz)**2 )**1.5,
+            -y / (x*x + y*y)**1.5 - (y - yz) / ( (x - xz)**2 + (y - yz)**2 )**1.5))
+
+    solver = scipy.integrate.ode(system).set_integrator("dopri5")
+    solver.set_initial_value(y0, t0)
+
+    planet = []
+    bypass = []
+
+    while solver.t < t1:
+        solver.integrate(solver.t + dt)
+        planet.append(numpy.array([solver.t, solver.y]))
+        bypass.append(numpy.array([solver.t, [bypassing_position(solver.t)]]))
+    
+    return numpy.array(planet), numpy.array(bypass)
+
+
+def plot_trajectory2(planet, bypass, phi):
+    fig = plt.figure(figsize=(8,7))
+    plt.scatter([0], [0], c="red")
+
+    xp = numpy.array([s[0] for s in planet[:,1]])
+    yp = numpy.array([s[1] for s in planet[:,1]])
+    xb = numpy.array([s[0][0] for s in bypass[:,1]])
+    yb = numpy.array([s[0][1] for s in bypass[:,1]])
+
+    plt.plot(xp, yp, linewidth=1)
+    plt.plot(xb, yb, linewidth=0.5)
+
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+    plt.title("Motion of a planet when star bypasses it ($\\varphi = {}\\pi$)".format(numpy.round(phi / scipy.constants.pi, 2)))
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    fig.savefig("images/task2_trajectory_phi={}.pdf".format(phi), bbox_inches="tight")
+    plt.show()
+
+
+def task2():
+    t0, t1 = 0, 100
+    phi = scipy.constants.pi / 6
+    y0 = [-numpy.sin(phi), numpy.cos(phi), numpy.cos(phi), numpy.sin(phi)]
+    dt = 0.01
+
+    planet, bypass = planetary_motion2(y0, t0, t1, dt)
+    plot_trajectory2(planet, bypass, phi)
+
+################################################################################
+#
+#       MAIN
+#
+################################################################################
 if __name__ == "__main__":
     os.chdir("hw1-ode-initial-value-problem/")
 
     # task1()
     # task1_err_ellipse()
     # task1_err_ellipses()
-    task1_err_to_dt()
+    # task1_err_to_dt()
+    # task1_circular_dt(dt=scipy.constants.pi/2)
+    # task1_stability()
+    # task1_small_ellipse()
+
+    # task2()
